74614c39a152ad4efdfd0d201988575b7f8a5a7e
adegilsontorres <149816665+adegilsontorres@users.noreply.github.com>
2024-11-19 23:47:14 -0300

    Update dado.js

--- app/dado.js
+++ app/dado.js
@@ -1,7 +1,48 @@
+const dado = document.querySelector('.dado')
+
 export default function jogarDado() {
     let valorSorteado = parseInt(Math.random()*6 +1)
-    alert(`Você tirou o número ${valorSorteado}`)
+    alert(`Você tirou ${valorSorteado} no dado!`)
+    
 
+    switch (valorSorteado) {
+        case 1:
+                dado.classList = 'dado';
+                dado.classList.add ('sorteado-1');
+            
+            break;
+            case 2:
+                dado.classList = 'dado';
+                dado.classList.add('sorteado-2');
+            
+            break;
+            case 3:
+                dado.classList = 'dado';
+                dado.classList.add('sorteado-3');
+            
+            break;
+            case 4:
+                dado.classList = 'dado';
+                dado.classList.add('sorteado-4');
+            
+            break;
+            case 5:
+                dado.classList = 'dado';
+                dado.classList.add('sorteado-5');
+                break;
+            case 6:
+                dado.classList = 'dado';
+                dado.classList.add('sorteado-6');
+            
+            break;
+    
+        default:
+            break;
+        
+    }
+
+    console.log(dado);
+    
     return valorSorteado
     
 }
